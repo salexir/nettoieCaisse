cb965789151216b7fe77abb587b9b13f21ece7c6
salexir <17128987+salexir@users.noreply.github.com>
2024-01-06 18:14:57 +0400

    Add function check_fileTypes

---
+++ R/file_checkers.R
@@ -0,0 +1,22 @@
+check_fileTypes <- function(file){
+
+  stopifnot("Financial files need to be csv" = tools::file_ext(file) == "csv")
+
+}
+
+
+
+check_fileNames <- function(file){
+
+  # small-ish data file of allowable file names
+
+
+}
+
+
+
+validate_inputFiles <- function(){
+
+
+
+}
