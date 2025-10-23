e343c632175173181bc4d79c8a8eac7b80345c8e
salexir <17128987+salexir@users.noreply.github.com>
2025-10-23 19:12:44 +0100

    Reframe unit tests to align with function role

--- tests/testthat/test-check_file_validity.R
+++ tests/testthat/test-check_file_validity.R
@@ -13,24 +13,69 @@ test_that("generated file names have the elements",{
 
 # 2.0 Column Validating Functions ==============================================
 
-test_that("Assert bankname read from file is not mangled",
+test_that("harmonise_dates() coerces correctly; YYYY-MM-DD",
           {
-            expect_equal(read_bankName_from_file("data/sdi/3940/TD-shared.csv"), expected = "TD")
+            dates <- c("2022-12-01", "2022-10-15")
+
+            expect_equal(as.character(harmonise_dates(dates)),
+                         c("2022-12-01", "2022-10-15"))
 
           })
 
-test_that("Assert bankname read from file is not mangled",
+test_that("harmonise_dates() coerces correctly; MM/DD/YYYY",
           {
-            expect_equal(read_bankName_from_file("data/sdi/3940/Revolut-shared2.csv"), expected = "Revolut")
+            dates <- c("03/01/2014", "12/23/2025")
+
+            expect_equal(as.character(harmonise_dates(dates)),
+                         c("2014-03-01", "2025-12-23"))
 
           })
 
-test_that("Assert bankname read from file is not mangled",
+test_that("harmonise debit column always returns positive values, given signed col",
           {
-            expect_equal(read_bankName_from_file("Revolut-shared2.csv"), expected = "Revolut")
+            expect_setequal(harmonise_debit_column(c(30, -12, 0), is_signed = TRUE),
+                            c(NA, 12, 0))
+          })
 
+
+test_that("harmonise debit column always returns positive values, given signed col",
+          {
+            expect_setequal(harmonise_debit_column(c(30, 12, 0), is_signed = FALSE),
+                            c(30, 12, 0))
+          })
+
+
+test_that("harmonise credit column always returns positive values, given signed col",
+          {
+            expect_setequal(harmonise_credit_column(c(30, -12, 0), is_signed = TRUE),
+                            c(30, NA, NA))
+          })
+
+
+test_that("harmonise debit column always returns positive values, given signed col",
+          {
+            expect_setequal(harmonise_credit_column(c(30, 12, 0), is_signed = FALSE),
+                            c(30, 12, 0))
           })
 # 3.0 Misc =====================================================================
+
+test_that("make_sentence_case returns... sentence case",
+          {
+            expect_equal(make_sentence_case("whaddup"), "Whaddup")
+          })
+
+test_that("make_sentence_case returns char type",
+          {
+          expect_type(make_sentence_case("whaddup"), type = "character")
+            }
+          )
+
+test_that("make_sentence_case returns obj of length 1",
+          {
+            expect_length(make_sentence_case("whaddup"), 1)
+          }
+)
+
 ### SUPERSEEDED
 #
 # test_that("Assert bankname read from file is not mangled",
